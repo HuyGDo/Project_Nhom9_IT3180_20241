60bf8a64f4fb6fa4f2bcbe5f74098166cc09c010
Hiep <tdhiep2712@gmail.com>
2024-10-24 13:32:55 +0700

    Fixed variable name error in db config

--- config/db/index.js
+++ config/db/index.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
-const { mongoDBURL } = require('../configuration');
+const { mongoDBUrl } = require('../configuration');
 
 async function connect(){
     try {
-        await mongoose.connect(mongoDBURL);
+        await mongoose.connect(mongoDBUrl);
         console.log("Connected to the DB successfully!");
     } catch (error) {
         console.log("Connection failure!: ", error);
