6dbbddfa950f5a81e7fc9f16d1e5b7cbde518f1e
Hiep <tdhiep2712@gmail.com>
2024-10-24 13:51:25 +0700

    Added Blog Controller

--- config/configuration.js
+++ config/configuration.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoDBUrl: '',
+    mongoDBUrl: 'mongodb://localhost:27017/',
     PORT: process.env.PORT || 3000,
     globalVariables: (req, res, next) => {
         next();
