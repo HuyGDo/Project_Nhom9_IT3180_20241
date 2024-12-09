b279c2781d3af5844a6fb99cd68858b173c27960
thaianh1210 <thaianh102004@gmail.com>
2024-12-09 22:09:14 +0700

    Clean

--- config/configuration.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    mongoDBUrl: 'mongodb://localhost:27017/BussinCookin',
-
-    PORT: process.env.PORT || 3000,
-    globalVariables: (req, res, next) => {
-        next();
-    },
-};
--- public/js/user.js
+++
@@ -1,1 +0,0 @@
-//Write frontend JS here
