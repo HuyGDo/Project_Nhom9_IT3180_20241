a35f13e16f8fc265fe2fef2f98e84a88b5ccee09
Tunn Tunn <onquangtung18052004@gmail.com>
2024-12-22 22:56:33 +0700

    fix scss

--- config/configuration.js
+++ config/configuration.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoDBUrl: 'mongodb://localhost:27017/BussinCookin',
+    mongoDBUrl: "mongodb://localhost:27017/BussinCookin",
 
     PORT: process.env.PORT || 3000,
     globalVariables: (req, res, next) => {
